c059f8eefc527a96e7bdbb2133cfe8ff42c79ae2
omvr <oabdelrazzik@gmail.com>
2024-10-20 15:00:53 +0300

    model deployment

--- model_deployment/customer_value_prediction/customer_value_prediction/settings.py
+++ model_deployment/customer_value_prediction/customer_value_prediction/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -117,9 +118,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = "static/"
+SRATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'customer_value_prediction/static')
+]
+
 
 # Default primary key field type
-# https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
+# https://docs.djangoroject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
